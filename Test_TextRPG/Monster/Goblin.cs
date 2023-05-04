d51a137b22dd0d29c2e96f13862801ee893ddb7a
Pass1948 <dldudtls100@naver.com>
2023-05-04 21:49:18 +0900

    초기 커밋.

---
+++ Test_TextRPG/Monster/Goblin.cs
@@ -0,0 +1,58 @@
+﻿using Project_TextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_TextRPG
+{
+    public class Goblin : Monster
+    {
+
+        public Goblin()
+        {
+            name = "고블린";
+            curHp = 15;
+            maxHp = 15;
+            ap = 5;
+            dp = 1;
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("####################");
+            sb.AppendLine("#                  #");
+            sb.AppendLine("#   (엄청난 고블린) #");
+            sb.AppendLine("#  (고블고블고블린) #");
+            sb.AppendLine("#                  #");
+            sb.AppendLine("####################");
+            image = sb.ToString();
+        }
+
+        private int moveTurn = 0;
+
+        public override void MoveAction()
+        {
+               switch(moveTurn++)
+                {
+                    case 0:
+                        TryMove(Direction.Left);
+                        break;
+                    case 1:
+                        TryMove(Direction.Right);
+                        break;
+                    case 2:
+                        TryMove(Direction.Right);
+                        break;
+                    case 3:
+                        TryMove(Direction.Left);
+                        break;
+                }
+            if (moveTurn == 4)
+            {
+                moveTurn = 0;
+            }
+            Position prevPos = pos;
+        }
+    }
+}
+
---
+++ Test_TextRPG/Monster/MvMush.cs
@@ -0,0 +1,50 @@
+﻿using Project_TextRPG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_TextRPG
+{
+    
+    public class MvMush : Monster
+    {
+        public MvMush()
+        {
+            name = "머쉬레인보우";
+            curHp = 5;
+            maxHp = 5;
+            ap = 0;
+            dp = 5;
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("####################");
+            sb.AppendLine("#                  #");
+            sb.AppendLine("#   (무지개 버섯)   #");
+            sb.AppendLine("#  (버섯버섯버섯링) #");
+            sb.AppendLine("#                  #");
+            sb.AppendLine("####################");
+            image = sb.ToString();
+        }
+
+        
+        private Random random = new Random();
+        private int moveTurn = 0;
+        public override void MoveAction()
+        {
+
+            
+
+            do
+            {
+                int x = random.Next(0, 5);
+                int y = random.Next(0, 5);
+
+               
+
+            } while (!Data.map[pos.y, pos.x] && Data.IsObjectInPos(pos)) ; 
+
+        }
+    }
+}
